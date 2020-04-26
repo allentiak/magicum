2f6cb0141def72a6da82dee7ef6962e864b65064
Antonio Alonzi <aalonzi@ebay.com>
2020-04-26 17:33:08 +0100

    Run Clojure Tests.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject magicum "0.1.0-SNAPSHOT"
+  :description "Magicum: ADD DESCRIPTION"
+  :url "https://github.com/allentiak/magicum"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :main magicum.specs)
