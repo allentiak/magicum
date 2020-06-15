0a03e06a30553910928e1795790b14bda7e73b65
Leandro Doctors <ldoctors@gmail.com>
2020-06-15 19:25:30 -0300

    fixup! add subtype->keywords fn

--- src/magicum/utils.clj
+++ src/magicum/utils.clj
@@ -4,8 +4,8 @@
 
 (defn subtypes->keywords
        [s]
-       "converts a seq of subtypes 'Abcd, Bedcu, ... Yhou, and Zordu' into a seq of keywords"
-       (map (comp keyword str/trim) (-> s
-                                      (str/lower-case)
-                                      (str/replace ", and " ", ")
-                                      (str/split #", "))))
+       "converts a seq of subtypes `Abcd, Bedcu, ..., Serra's Realm, ..., and Zordu` into a seq of keywords"
+       (map (comp keyword (partial str/replace " " "-") str/trim) (-> s
+                                                                    (str/replace ", and " ", ")
+                                                                    (str/lower-case)
+                                                                    (str/split #","))))
