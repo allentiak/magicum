c96000f27304564798bb7eed576fde1818e5e6d9
Leandro Doctors <ldoctors@gmail.com>
2020-06-17 16:10:23 -0300

    rules: start integrating old code into new one
    Also remove useless "main"

--- src/magicum/specs.clj
+++ src/magicum/specs.clj
@@ -175,6 +175,66 @@
     :version "2020.06.01"}
   ::counter-type #{:plus-or-minus-counter :keyword-counter :loyalty-counter :poison-counter})
 
+
+
+(s/def ::artist (s/and string? seq))
+(s/def ::set (s/and string? seq))
+(s/def ::set-number pos-int?)
+(s/def ::metadata (s/keys :req-un [::set ::set-number ::artist]))
+;; (s/exercise ::metadata)
+(s/def ::name (s/and string? seq))
+(s/def ::types (s/and set? (s/+ ::type)))
+
+(s/exercise ::types)
+
+(s/def ::sub-type ::type)
+(s/def ::rules string?)
+(s/def ::legendary? boolean?)
+(s/def ::world? boolean?)
+
+(s/def ::base-card (s/keys :req-un [::name ::types ::metadata ::rules]
+                           :opt-un [::sub-type ::legendary? ::world?]))
+
+(->> (s/exercise ::base-card) (take 5))
+
+(s/def ::amount pos-int?)
+(s/def ::color #{:white :blue :black :red :green})
+(s/def ::colors (s/+ ::color))
+
+(s/def ::mana (s/keys :req-un [::amount]
+                      :opt-un [::colors]))
+(s/def ::manas (s/+ ::mana))
+(s/def ::cost (s/* ::manas))
+
+(s/def ::spell (s/and ::base-card (s/keys :req-un [::cost])))
+
+(s/def ::power int?)
+(s/def ::toughness int?)
+
+(s/def ::creature (s/and ::spell (s/keys :req-un [::power ::toughness])))
+
+(s/def ::loyalty int?)
+
+(s/def ::planeswalker (s/and ::spell (s/keys :req-un [::loyalty])))
+
+(defmulti card-spec :types)
+
+(defmethod card-spec #{:land} [_] ::base-card)
+(defmethod card-spec #{:creature} [_] ::creature)
+(defmethod card-spec #{:artifact} [_] ::spell)
+(defmethod card-spec #{:enchantment} [_] ::spell)
+(defmethod card-spec #{:sorcery} [_] ::spell)
+(defmethod card-spec #{:instant} [_] ::spell)
+(defmethod card-spec #{:planeswalker} [_] ::planeswalker)
+(defmethod card-spec #{:artifact :creature} [_] ::creature)
+(defmethod card-spec #{:enchantment :creature} [_] ::creature)
+(defmethod card-spec #{:artifact :enchantment} [_] ::spell)
+
+(s/def ::card (s/multi-spec card-spec :types))
+
+
+
+
 (s/def
   ^{:rule "205.2a"
     :version "2020.06.01"}
@@ -226,64 +286,3 @@
   ^{:rule "205.4a"
     :version "2020.06.01"}
   ::supertype #{:basic :legendary :ongoing :snow :world})
-
-
-****************** OLD CODE HERE **************
-
-(s/def ::artist (s/and string? seq))
-(s/def ::set (s/and string? seq))
-(s/def ::set-number pos-int?)
-(s/def ::metadata (s/keys :req-un [::set ::set-number ::artist]))
-;; (s/exercise ::metadata)
-(s/def ::name (s/and string? seq))
-(s/def ::types (s/and set? (s/+ ::type)))
-(s/exercise ::types)
-(s/def ::sub-type ::type)
-(s/def ::rules string?)
-(s/def ::legendary? boolean?)
-(s/def ::world? boolean?)
-
-(s/def ::base-card (s/keys :req-un [::name ::types ::metadata ::rules]
-                           :opt-un [::sub-type ::legendary? ::world?]))
-
-(->> (s/exercise ::base-card) (take 5))
-
-(s/def ::amount pos-int?)
-(s/def ::color #{:white :blue :black :red :green})
-(s/def ::colors (s/+ ::color))
-
-(s/def ::mana (s/keys :req-un [::amount]
-                      :opt-un [::colors]))
-(s/def ::manas (s/+ ::mana))
-(s/def ::cost (s/* ::manas))
-
-(s/def ::spell (s/and ::base-card (s/keys :req-un [::cost])))
-
-(s/def ::power int?)
-(s/def ::toughness int?)
-
-(s/def ::creature (s/and ::spell (s/keys :req-un [::power ::toughness])))
-
-(s/def ::loyalty int?)
-
-(s/def ::planeswalker (s/and ::spell (s/keys :req-un [::loyalty])))
-
-(defmulti card-spec :types)
-
-(defmethod card-spec #{:land} [_] ::base-card)
-(defmethod card-spec #{:creature} [_] ::creature)
-(defmethod card-spec #{:artifact} [_] ::spell)
-(defmethod card-spec #{:enchantment} [_] ::spell)
-(defmethod card-spec #{:sorcery} [_] ::spell)
-(defmethod card-spec #{:instant} [_] ::spell)
-(defmethod card-spec #{:planeswalker} [_] ::planeswalker)
-(defmethod card-spec #{:artifact :creature} [_] ::creature)
-(defmethod card-spec #{:enchantment :creature} [_] ::creature)
-(defmethod card-spec #{:artifact :enchantment} [_] ::spell)
-
-(s/def ::card (s/multi-spec card-spec :types))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
