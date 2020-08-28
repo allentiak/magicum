4422ac7d0483ae89003a691cfe3068d736bb7c4a
Leandro Doctors <ldoctors@gmail.com>
2020-08-27 22:03:17 -0300

    specs: we will skip zone manipulation for now

--- test/magicum/specs_test.clj
+++ test/magicum/specs_test.clj
@@ -5,11 +5,11 @@
 
 (set! *warn-on-reflection* true)
 
-(testing "manipulating zones"
-  (let [card {:card-name "My new card" :card-legal-text "This legal text is supposed to do something. However, it does nothing."}
-        non-empty-zone (list card)
-        empty-zone (list nil)]
-    (is (= empty-zone (remove-card-from-zone card non-empty-zone)))))
+;; (testing "manipulating zones"
+;;   (let [card {:card-name "My new card" :card-legal-text "This legal text is supposed to do something. However, it does nothing."}
+;;         non-empty-zone (list card)
+;;         empty-zone (list nil)]
+;;     (is (= empty-zone (remove-card-from-zone card non-empty-zone)))))
 
 (testing "world consistency"
   (let [card {:card-name "My new card" :card-legal-text "This legal text is supposed to do something. However, it does nothing."}
