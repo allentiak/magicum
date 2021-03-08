d2b8ef5044fb46fd2fc114f12d1736f0192c0f2b
Leandro Doctors <ldoctors@gmail.com>
2021-03-08 17:16:14 -0300

    fixup! rules: update to 2021.02.15 (filename: 2021.02.24)

--- src/magicum/specs/refactor_me.clj
+++ src/magicum/specs/refactor_me.clj
@@ -457,7 +457,7 @@
 
 ;; 603: triggered abilities
 (s/def
-  ::every-time-an-event-occurs: (::determine-appliable-triggered-abilities ::activate-appliable-triggered-abilities))
+  ::every-time-an-event-occurs (::determine-appliable-triggered-abilities ::activate-appliable-triggered-abilities))
 
 ;; TODO: 603.10: triggered abilities that look back in time
 
@@ -514,7 +514,7 @@
 
 ;; 702: keyword abilities
 (s/def
-  ::keyword-ability #{::deathtouch ::defender ::double-strike ::enchant ::encore ::equip ::first-strike ::flash ::flying ::haste ::hexproof ::indestructible ::intimidate ::landwalk ::lifelink ::protection ::reach ::shroud ::trample ::vigilance ::banding ::rampage ::cumulative-upkeep ::flanking ::phasing ::buyback ::shadow ::cycling ::echo ::horsemanship ::fading ::kicker ::flashback ::madness ::fear ::morph ::amplify ::provoke ::storm ::affinity ::entwine ::modular ::sunburst ::bushido :soulshift ::splice ::offering ::ninjutsu ::epic ::convoke ::dredge ::transmute ::bloodthirst ::haunt ::replicate ::forecast ::graft ::recover ::ripple ::split-second ::suspend ::vanishing ::absorb ::aura-swap ::delve ::fortify ::frenzy ::gravestorm ::poisonous ::transfigure ::champion ::changeling ::evoke ::hideaway ::prowl ::reinforce ::conspire ::persist ::wither ::retrace ::devour ::exalted ::unearth ::cascade ::annihilator ::level-up ::rebound ::totem-armor ::infect ::battle-cry ::living-weapon ::undying ::miracle ::soulbound ::overload ::scavenge ::unleash ::cipher ::evolve ::extort ::fuse ::bestow ::tribute ::dethrone ::hidden-agenda ::outlast ::prowess ::dash ::exploit ::menace ::renown ::awaken ::devoid ::ingest ::myriad ::surge ::skulk ::emerge ::escalate ::melee ::crew ::fabricate ::partner ::undaunted ::improvise ::aftermath ::embalm ::eternalize ::afflict ::assist :jump-start ::mentor ::afterlife ::riot ::spectacle ::escape ::companion ::mutate ::encore ::boast ::foretell})
+  ::keyword-ability #{::deathtouch ::defender ::double-strike ::enchant ::equip ::first-strike ::flash ::flying ::haste ::hexproof ::indestructible ::intimidate ::landwalk ::lifelink ::protection ::reach ::shroud ::trample ::vigilance ::banding ::rampage ::cumulative-upkeep ::flanking ::phasing ::buyback ::shadow ::cycling ::echo ::horsemanship ::fading ::kicker ::flashback ::madness ::fear ::morph ::amplify ::provoke ::storm ::affinity ::entwine ::modular ::sunburst ::bushido :soulshift ::splice ::offering ::ninjutsu ::epic ::convoke ::dredge ::transmute ::bloodthirst ::haunt ::replicate ::forecast ::graft ::recover ::ripple ::split-second ::suspend ::vanishing ::absorb ::aura-swap ::delve ::fortify ::frenzy ::gravestorm ::poisonous ::transfigure ::champion ::changeling ::evoke ::hideaway ::prowl ::reinforce ::conspire ::persist ::wither ::retrace ::devour ::exalted ::unearth ::cascade ::annihilator ::level-up ::rebound ::totem-armor ::infect ::battle-cry ::living-weapon ::undying ::miracle ::soulbound ::overload ::scavenge ::unleash ::cipher ::evolve ::extort ::fuse ::bestow ::tribute ::dethrone ::hidden-agenda ::outlast ::prowess ::dash ::exploit ::menace ::renown ::awaken ::devoid ::ingest ::myriad ::surge ::skulk ::emerge ::escalate ::melee ::crew ::fabricate ::partner ::undaunted ::improvise ::aftermath ::embalm ::eternalize ::afflict ::assist :jump-start ::mentor ::afterlife ::riot ::spectacle ::escape ::companion ::mutate ::encore ::boast ::foretell})
 
 ;; 703: stated-based actions
 ;; 704: turn-based actions
